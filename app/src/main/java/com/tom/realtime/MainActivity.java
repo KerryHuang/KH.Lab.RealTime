3ef9b59245de68aebfd121ec1d75d09505b2996a
KerryHuang <zihao317@gmail.com>
2016-05-23 21:18:53 +0800

    抽獎

---
+++ app/src/main/java/com/tom/realtime/MainActivity.java
@@ -0,0 +1,140 @@
+package com.tom.realtime;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Random random = new Random();
+    private AlertDialog dialog;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
+        Firebase.setAndroidContext(this);
+        String url = "https://project-7931157122344089396.firebaseio.com/pool";
+
+        new Firebase(url).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String name = (String) dataSnapshot.child("name").getValue();
+                adapter.add(name);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int count = adapter.getCount();
+                final int num = random.nextInt(count);
+                dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("抽獎中")
+                        .setMessage(adapter.getItem(num))
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                adapter.remove(adapter.getItem(num));
+                            }
+                        }).show();
+                new DrawingTask().execute(adapter.getCount());
+            }
+        });
+    }
+
+    class DrawingTask extends AsyncTask<Integer, Integer, Integer>{
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            int r = 0;
+            for (int i=0;i<100;i++){
+                r = random.nextInt(params[0]);
+                publishProgress(r);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return r;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            dialog.setMessage(adapter.getItem(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            dialog.setTitle("中獎人");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
